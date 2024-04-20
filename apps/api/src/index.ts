a25061d054cc9900600c9ab371c2a8ba9b7c58d9
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-20 18:38:49 -0300

    refactor(api-index): updating ability access by user role

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,6 @@
-import { ability } from '@repo/auth'
+import { defineAbilityFor } from '@repo/auth'
+
+const ability = defineAbilityFor({ role: 'MEMBER' })
 
 const userCanInviteSomenoneElse = ability.can('invite', 'User')
 const userCanDeleteSomeoneElse = ability.can('delete', 'User')
