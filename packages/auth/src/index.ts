9523af5a35dca8530e170e0fd0497d95af61c65d
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-20 18:38:18 -0300

    refactor(auth-index): adding defineAbilityFor function to handle ability

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -6,6 +6,9 @@ import {
   MongoAbility,
 } from '@casl/ability'
 
+import { User } from './models/user'
+import { permissions } from './permissions'
+
 const actions = ['manage', 'invite', 'delete'] as const
 const subjects = ['User', 'all'] as const
 type AppAbilities = [
@@ -19,8 +22,16 @@ type AppAbilities = [
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`No permissions defined for role ${user.role}`)
+  }
 
-export const ability = build()
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
