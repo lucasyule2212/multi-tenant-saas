4b26b3290f89337eba33f502491c7ca857362cd8
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-26 10:14:05 -0300

    refactor(create-account-route): updating create-account route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   // ? Zod validation integrated with Fastify, allows to validate the request body with Zod schema middleware
@@ -15,8 +18,30 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User created'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userWithEmail) {
+        return reply.status(400).send({
+          error: 'User with same e-mail already exists.',
+        })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send(user)
     },
   )
 }
