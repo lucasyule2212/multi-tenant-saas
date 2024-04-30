9152e4b060a4037eb34d7d620c2b6e18aa8735b8
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-29 23:18:55 -0300

    refactor(routes-http): signin jwt token and retrieving to user on login

--- apps/api/src/http/routes/auth/authenticate-with-password.ts
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -48,7 +48,16 @@ export async function authenticateWithPassword(app: FastifyInstance) {
         })
       }
 
-      return 'Authenticated!'
+      const token = await reply.jwtSign(
+        { sub: userFromEmail.id },
+        {
+          sign: {
+            expiresIn: '7d',
+          },
+        },
+      )
+
+      return reply.status(201).send({ token })
     },
   )
 }
