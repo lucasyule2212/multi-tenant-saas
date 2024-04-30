387bfc00c3316edb525b52f3b0cfc830b52a0fcc
Lucas Yule <lyrma@cin.ufpe.br>
2024-04-29 23:11:16 -0300

    feat(routes-auth): adding authenticate-with-password route

---
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -0,0 +1,54 @@
+import bcrypt from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function authenticateWithPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/pasword',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Authenticate with email & password',
+        body: z.object({
+          email: z.string().email(),
+          password: z.string().min(8),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!userFromEmail) {
+        return reply.status(401).send({
+          message: 'Invalid credentials',
+        })
+      }
+
+      if (userFromEmail.passwordHash === null) {
+        return reply.status(400).send({
+          message: 'User does not have a password. Use social login instead.',
+        })
+      }
+
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        userFromEmail.passwordHash,
+      )
+
+      if (!isPasswordValid) {
+        return reply.status(401).send({
+          message: 'Invalid credentials',
+        })
+      }
+
+      return 'Authenticated!'
+    },
+  )
+}
