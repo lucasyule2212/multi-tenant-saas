1c7ca5ca8ced12f9954862e17fb2dd5f765568be
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-01 23:00:45 -0300

    refactor(routes): registering new routes

--- apps/api/src/http/routes/routes.ts
+++ apps/api/src/http/routes/routes.ts
@@ -2,8 +2,12 @@ import { app } from '../server'
 import { authenticateWithPassword } from './auth/authenticate-with-password'
 import { createAccount } from './auth/create-account'
 import { getUserProfile } from './auth/get-user-profile'
+import { requestPasswordRecover } from './auth/request-password-recover'
+import { resetUserPassword } from './auth/reset-user-password'
 // ? Import the routes
 
 app.register(createAccount)
 app.register(authenticateWithPassword)
 app.register(getUserProfile)
+app.register(requestPasswordRecover)
+app.register(resetUserPassword)
