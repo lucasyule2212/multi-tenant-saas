98433b527b5d81bcdf55fb71e8450d6f6de8c223
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-01 23:00:11 -0300

    feat(reset-user-password): adding password reset route

---
+++ apps/api/src/http/routes/auth/reset-user-password.ts
@@ -0,0 +1,54 @@
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function resetUserPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/reset',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Reset user password',
+        body: z.object({
+          code: z.string(),
+          newPassword: z.string().min(8),
+        }),
+        response: {
+          204: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { code, newPassword } = request.body
+
+      const token = await prisma.token.findUnique({
+        where: {
+          id: code,
+        },
+      })
+
+      if (!token) {
+        throw new UnauthorizedError('Invalid token')
+      }
+
+      const passwordHash = await hash(newPassword, 6)
+
+      await prisma.user.update({
+        where: {
+          id: token.userId,
+        },
+        data: {
+          passwordHash,
+        },
+      })
+
+      // ? 204 is the status for successful requests with no content
+      return reply.status(204).send()
+    },
+  )
+}
