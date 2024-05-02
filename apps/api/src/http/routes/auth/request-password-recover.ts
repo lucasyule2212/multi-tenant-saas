291e7709008a96e69810854569dd02e58fc0fa0e
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-01 22:59:55 -0300

    feat(request-password-recover): adding reset token recover route

---
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecover(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Request password recover',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) {
+        // ? We don't want to leak information about the existence of the user
+        return reply.status(201).send()
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      // ? Send email with the code
+      console.log('Recover password token: ', code)
+
+      return reply.status(201).send()
+    },
+  )
+}
