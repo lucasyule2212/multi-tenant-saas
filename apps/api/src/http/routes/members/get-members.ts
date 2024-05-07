29b297eb1ec277d2fb386bcf94bf4282def68fd9
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-07 11:12:49 -0300

    fix(get-members): fixing route endpoint

--- apps/api/src/http/routes/members/get-members.ts
+++ apps/api/src/http/routes/members/get-members.ts
@@ -14,7 +14,7 @@ export async function getMembers(app: FastifyInstance) {
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
     .get(
-      '/organizations/:orgSlug',
+      '/organizations/:orgSlug/members',
       {
         schema: {
           tags: ['Members'],
