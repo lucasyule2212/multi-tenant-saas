acb1fb005b9fed2e141aa2af7bd1bf0f910b0748
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-08 20:42:16 -0300

    fix(schema.prisma): fixing onDelete methods from relations

---
+++ apps/api/prisma/migrations/20240508234144_updating_relations_ondelete_method/migration.sql
@@ -0,0 +1,35 @@
+-- DropForeignKey
+ALTER TABLE "accounts" DROP CONSTRAINT "accounts_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "invites" DROP CONSTRAINT "invites_organizationId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "members" DROP CONSTRAINT "members_organizationId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "members" DROP CONSTRAINT "members_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "projects" DROP CONSTRAINT "projects_organizationId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tokens" DROP CONSTRAINT "tokens_user_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "tokens" ADD CONSTRAINT "tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "invites" ADD CONSTRAINT "invites_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "members" ADD CONSTRAINT "members_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "members" ADD CONSTRAINT "members_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "projects" ADD CONSTRAINT "projects_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
